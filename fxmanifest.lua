7fe7780f15158238022731c127b1ee1db033fc9a
notstatic <75686734+notstatic@users.noreply.github.com>
2021-03-23 11:30:13 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,6 +6,5 @@ author 'Not Static'
 description 'Pelaajan seuranta mm jostain sääntörikkeestä jne.'
 version '1.0'
 
-client_script 'c.lua'
 server_script '@mysql-async/lib/MySQL.lua'
 server_script 's.lua'
