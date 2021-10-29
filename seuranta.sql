6707bd3e84126a4f2eb4ac517c4ca0fdb4c009c1
notstatic <75686734+notstatic@users.noreply.github.com>
2021-10-29 20:44:38 +0300

    Update seuranta.sql
    - Translated from finnish to english
    - Removed collates

--- seuranta.sql
+++ seuranta.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS `seuranta` (
+CREATE TABLE IF NOT EXISTS `player_tracking` (
   `hex` varchar(50) DEFAULT NULL,
-  `syy` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `submitter` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `reason` longtext CHARACTER SET utf8mb4,
+  `submitter` longtext CHARACTER SET utf8mb4
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
